a50c2e3d08285adc207443535ef9127da14c6c9f
Daren-Ken St-laurent <Daren511@hotmail.com>
2014-12-04 13:24:37 -0500

    j'ai rajouter des commentaires

--- Demo2/Demo2/Form1.cs
+++ Demo2/Demo2/Form1.cs
@@ -16,5 +16,10 @@ namespace Demo2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ///BLABLBALBA
+        }
     }
 }
--- Demo2/Demo2/Program.cs
+++ Demo2/Demo2/Program.cs
@@ -18,5 +18,6 @@ namespace Demo2
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
     }
 }
